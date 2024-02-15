825c658506105d36175ae967d9508614513d70e8
Eric <126840543+eccol@users.noreply.github.com>
2024-02-15 14:44:45 -0500

    Create 12 cards

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,10 +1,17 @@
 import Card from "./Card.jsx";
 
+const NUMBER_OF_CARDS = 12;
+
 export default function Board() {
+  const cards = [];
+  for (let i = 0; i < NUMBER_OF_CARDS; i++) {
+    cards.push(<Card />);
+  }
+
   return (
     <div className="board">
       <h1>Board</h1>
-      <Card />
+      {cards}
     </div>
   );
 }
